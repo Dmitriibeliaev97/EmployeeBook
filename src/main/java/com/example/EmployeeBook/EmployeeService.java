b3f1097bf5e644a1cbfc7bd219a2ca1e941e1f65
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-04-25 13:02:15 +0300

    Initial commit

---
+++ src/main/java/com/example/EmployeeBook/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeBook;
+
+import java.util.Collection;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstName, String lastName);
+
+    Employee removeEmployee(String firstName, String lastName);
+
+    Employee findEmployee(String firstName, String lastName);
+
+    Collection<Employee> findeAll();
+}
---
+++ src/main/java/com/example/EmployeeBook/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.EmployeeBook;
+
+import com.example.EmployeeBook.Exceptions.EmployeeAlreadyAddedException;
+import com.example.EmployeeBook.Exceptions.EmployeeNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final List<Employee> employeeBook;
+
+    public EmployeeServiceImpl() {
+        this.employeeBook = new ArrayList<>();
+    }
+
+    @Override
+    public Employee addEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeBook.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        employeeBook.add(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee removeEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeBook.contains(employee)) {
+            employeeBook.remove(employee);
+            return employee;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Employee findEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeBook.contains(employee)) {
+            return employee;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Collection<Employee> findeAll() {
+        return Collections.unmodifiableList(employeeBook);
+    }
+}
