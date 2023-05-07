0175b79aab499387c2bd00ec8820924200b632ab
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-05-07 12:20:55 +0300

    Добавлены исправления

--- src/main/java/com/example/EmployeeBook/EmployeeServiceImpl.java
+++ src/main/java/com/example/EmployeeBook/EmployeeServiceImpl.java
@@ -2,24 +2,24 @@ package com.example.EmployeeBook;
 
 import com.example.EmployeeBook.Exceptions.EmployeeAlreadyAddedException;
 import com.example.EmployeeBook.Exceptions.EmployeeNotFoundException;
+import com.example.EmployeeBook.Exceptions.EmployeeStorageIsFullException;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-
-    private final Map<String, Employee> employeeBook;
-
-    public EmployeeServiceImpl() {
-        this.employeeBook = new HashMap<>();
-    }
+    private final int EMPLOYEEBOOK_STORAGE_SIZE = 5;
+    private final Map<String, Employee> employeeBook = new HashMap<>();
 
     @Override
     public Employee addEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
         if (employeeBook.containsKey(employee.getFullName())) {
-            throw new EmployeeAlreadyAddedException();
+            throw new EmployeeAlreadyAddedException("Сотрудник уже добавлен");
+        }
+        if (employeeBook.size() == EMPLOYEEBOOK_STORAGE_SIZE) {
+            throw new EmployeeStorageIsFullException("Хранилище полное");
         }
         employeeBook.put(employee.getFullName(), employee);
         return employee;
@@ -31,7 +31,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         if (employeeBook.containsKey(employee.getFullName())) {
             return employeeBook.remove(employee.getFullName());
         }
-        throw new EmployeeNotFoundException();
+        throw new EmployeeNotFoundException("Сотрудник не найден");
     }
 
     @Override
@@ -40,11 +40,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         if (employeeBook.containsKey(employee.getFullName())) {
             return employeeBook.get(employee.getFullName());
         }
-        throw new EmployeeNotFoundException();
+        throw new EmployeeNotFoundException("Сотрудник не найден");
     }
 
 
     @Override
-    public Collection<Employee> findAll() { return Collections.unmodifiableCollection(employeeBook.values());
+    public Collection<Employee> findAll() {
+        return Collections.unmodifiableCollection(employeeBook.values());
     }
 }
