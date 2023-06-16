f71c540eb37e26830c171c04067692b55ceb9860
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-06-16 23:17:50 +0300

    Полностью покрыт Employee Service unit тестами

--- src/test/java/com/example/EmployeeBook/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/example/EmployeeBook/service/impl/EmployeeServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.example.EmployeeBook.service.impl;
 
+import com.example.EmployeeBook.Exceptions.EmployeeAlreadyAddedException;
+import com.example.EmployeeBook.Exceptions.EmployeeNotFoundException;
+import com.example.EmployeeBook.Exceptions.EmployeeStorageIsFullException;
 import com.example.EmployeeBook.model.Employee;
 import com.example.EmployeeBook.service.EmployeeService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class EmployeeServiceImplTest {
     private Employee employeeDmitrii;
@@ -33,35 +37,48 @@ class EmployeeServiceImplTest {
 
 
     @Test
-    void addEmployee() {
+    void shouldAddEmployeeIfThereIsNone() {
         Employee expected = employees.addEmployee("Ivan", "Ivanov", 100000, 3);
-
         Employee actual = new Employee("Ivan", "Ivanov", 100000, 3);
-
         assertEquals(expected, actual);
-
     }
 
     @Test
-    void removeEmployee() {
+    void shouldThrowExceptionWhenEmployeeIsAlreadyAdded() {
+        assertThrows(EmployeeAlreadyAddedException.class, () -> employees.addEmployee("Dmitrii", "Beliaev", 150000, 1));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenStorageIsFull() {
+        employees.addEmployee("Petr", "Petrov", 110000, 3);
+        assertThrows(EmployeeStorageIsFullException.class, () -> employees.addEmployee("Ivan", "Ivanov", 100000, 3));
+    }
+
+    @Test
+    void shouldRemoveEmployee() {
         Employee expected = employees.removeEmployee("Dmitrii", "Beliaev");
-
         Employee actual = new Employee("Dmitrii", "Beliaev", 150000, 1);
-
         assertEquals(expected, actual);
+    }
+    @Test
+    void shouldThrowExceptionWhenEmployeeIsNotFoundWhenRemoveEmployee(){
+        assertThrows(EmployeeNotFoundException.class, () -> employees.removeEmployee("Petr", "Petrov"));
     }
 
     @Test
-    void findEmployee() {
+    void shouldFindEmployee() {
         Employee expected = employees.findEmployee("Dmitrii", "Beliaev");
-
         Employee actual = new Employee("Dmitrii", "Beliaev", 150000, 1);
-
         assertEquals(expected, actual);
     }
 
     @Test
-    void getAllEmployees() {
+    void shouldThrowExceptionWhenEmployeeIsNotFoundWhenFindEmployee() {
+        assertThrows(EmployeeNotFoundException.class, () -> employees.findEmployee("Petr", "Petrov"));
+    }
+
+    @Test
+    void shouldGetAllEmployees() {
         Map<String, Employee> expected = employees.getAllEmployees();
 
         Map<String, Employee> actual = new HashMap<>();
